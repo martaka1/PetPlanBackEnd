4780c36dee96aa6845d7feb926cd4cc2b02a74f9
martaka1 <130301958+martaka1@users.noreply.github.com>
2024-05-30 11:52:04 +0300

    Update AppDbContextFactory.cs

--- App.DAL.EF/AppDbContextFactory.cs
+++ App.DAL.EF/AppDbContextFactory.cs
@@ -8,7 +8,7 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     public AppDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseNpgsql("Host=localhost;Port=7890;Database=petplan;Username=postgres;Password=postgres");
+        optionsBuilder.UseNpgsql("$connectionString");
 
         return new AppDbContext(optionsBuilder.Options);
     }
